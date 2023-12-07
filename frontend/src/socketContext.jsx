cfb2402f8b02d3bc1a0222b9fee55d8bebbf0eb1
shailesh-25204 <2020bit035@sggs.ac.in>
2023-12-07 14:22:53 +0530

    Changed Server address

--- frontend/src/socketContext.jsx
+++ frontend/src/socketContext.jsx
@@ -4,7 +4,7 @@ import  Peer from 'simple-peer';
 
 const SocketContext = createContext();
 
-const socket = io('http://localhost:5000');
+const socket = io('https://medlink-iota.vercel.app/');
 
 const ContextProvider = ({children}) => {
     const [me,setMe] = useState("")
